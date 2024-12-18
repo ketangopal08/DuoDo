a01079ab8ced271d8085ebfaa9153247529ecc59
ketangopal08 <ketangopal08@gmail.com>
2024-12-18 21:49:29 +0530

    added new files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,40 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt','@nuxtjs/color-mode'],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt','@vueuse/motion/nuxt'],
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.css',
     configPath: '~/tailwind.config.ts',
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+  runtimeConfig: {
+    public: {
+      motion: {
+        directives: {
+          'pop-bottom': {
+            initial: {
+              scale: 0,
+              opacity: 0,
+              y: 100,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              y: 0,
+            }
+          }
+        }
+      }
+    }
   }
 })
